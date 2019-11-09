d4838b73bc642f78b9a71d1f5fc511b64f27daa9
Joe Meyer <joelawm@gmail.com>
2019-11-09 02:04:44 -0600

    Added functionality to MainMenu
    -Added script for MainMenu
    -Moved temp models from root to in engine.

---
+++ Hexapunk/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("\tNew Game option has been clicked.");
+        SceneManager.LoadScene(1);
+        Debug.Log("\tLoaded a new game.");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("\tGame has been quit.");
+        Application.Quit();
+    }
+}
